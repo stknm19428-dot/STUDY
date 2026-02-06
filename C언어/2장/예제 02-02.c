5741c48b83731014a888e3249e4e67dbece77e6e
stknm19428-dot <stknm19428@gmail.com>
2026-02-07 02:16:48 +0900

    Add files via upload

---
+++ C언어/2장/예제 02-02.c
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>  //stdio.h 파일의 내용을 포함시킴
+
+int main(void)
+{
+    printf("Be happy");  // "Be happy" 문자열을 출력함
+    printf("My friend");  // "My friend" 문자열을 출력함
+
+    return 0;
+}
