00e99d62eb9ece13d06ae0df3fe823aca7a8b793
stknm19428-dot <stknm19428@gmail.com>
2026-02-14 09:17:01 +0900

    Delete 코딩테스트/코드업/C언어 directory

--- 코딩테스트/코드업/C언어/1006.c
+++
@@ -1,8 +0,0 @@
-﻿#include <stdio.h>  
-
-int main(void)
-{
-    printf("\"!@#$%%^&*()\"");
-
-    return 0;
-}
--- 코딩테스트/코드업/C언어/1007.c
+++
@@ -1,8 +0,0 @@
-﻿#include <stdio.h>
-
-int main(void)
-{
-    printf("\"C:\\Download\\hello.cpp\"");
-
-    return 0;
-}
