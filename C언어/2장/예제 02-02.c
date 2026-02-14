4bb35dca67a7f1c45071b76e7ac084d4bd834fba
stknm19428-dot <stknm19428@gmail.com>
2026-02-14 09:16:50 +0900

    Delete C언어 directory

--- C언어/2장/예제 02-02.c
+++
@@ -1,9 +0,0 @@
-﻿#include <stdio.h>  //stdio.h 파일의 내용을 포함시킴
-
-int main(void)
-{
-    printf("Be happy");  // "Be happy" 문자열을 출력함
-    printf("My friend");  // "My friend" 문자열을 출력함
-
-    return 0;
-}
