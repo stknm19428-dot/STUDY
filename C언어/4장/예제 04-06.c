ac24df177c4c2cda713294900d467e3c6bbb14ba
stknm19428-dot <stknm19428@gmail.com>
2026-02-19 05:40:05 +0900

    Add files via upload

---
+++ C언어/4장/예제 04-06.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    int a = 30;
+    int res;
+
+    res = (a > 10) && (a < 20);  /*조항과 우항이 모두 참이면 참*/
+    printf("res=(a > 10) && (a < 20) : %d\n", res);
+
+    res = (a < 10) || (a > 20);  /*좌항과 우항 중 하나라도 참이면 참*/
+    printf("res=(a < 10) || (a > 20) : %d\n", res);
+
+    res = !(a >= 30);  /*거짓이면 참, 참이면 거짓*/
+    printf("res = !(a>=30) : %d\n", res);
+
+    return 0;
+}
