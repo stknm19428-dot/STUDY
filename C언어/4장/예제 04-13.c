493528700a50dd532958393c5cc244f0f00c8be5
stknm19428-dot <stknm19428@gmail.com>
2026-02-20 10:05:53 +0900

    Add files via upload

---
+++ C언어/4장/예제 04-13.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    int a = 10;  /*00000000 00000000 00000000 00001010*/
+    int b = 12;  /*00000000 00000000 00000000 00001100*/
+
+    printf("a & b : %d\n", a & b);
+    printf("a ^ b : %d\n", a ^ b);
+    printf("a | b : %d\n", a | b);
+    printf("~a : %d\n", ~a);
+    printf("a << 1 : %d\n", a << 1);
+    printf("a >> 2 : %d\n", a >> 2);
+
+    return 0;
+}
